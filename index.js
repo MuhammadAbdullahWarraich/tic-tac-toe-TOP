5f113518370152bc579ed6bf4ad7c557ae7ff544
MuhammadAbdullahWarraich <muhammadabdullah2016pk@gmail.com>
2024-07-17 23:50:27 +0500

    fix: replate old file with rewrite

--- index.js
+++ index.js
@@ -1,183 +1,273 @@
-// setting up players
-function Player(name, symbol) {
-    this.name = name;
-    this.symbol = symbol;
-}
-Player.prototype.getName = function() {
-    return this.name;
-    // if javascript is unable to find name in the entire prototypal chain of the calling object, it will return undefined
-}
-Player.prototype.changeName = function(newName) {
-    this.name = newName;
-}
-Player.prototype.getSymbol = function() {
-    return this.symbol;
-}
-const players = [
-    new Player("player1", " x  "),
-    new Player("player2", " o  "),
-];
-let currPlayer = players[0];
-// setting up game board
-const dimensions = 3;
-const gameboard = new Array(dimensions);
-for (let i = 0; i < dimensions; i++) {
-    gameboard[i] = new Array(dimensions);
-    for (let j = 0; j < dimensions; j++) {
-        gameboard[i][j] = null;
-    }
-}
-// setting up game status flag (win/loss, draw, running)
-let gameStatus = 0;
-// game
-function printBoard() {
-    let str = "";
-    for (let i = 0; i < dimensions; i++) {
-        for (let j = 0; j < dimensions; j++) {
-            str += gameboard[i][j];
-            str += "\t";
+function GameboardFactory_onetime() {
+    // ----------nested functions
+    function CellFactory() {
+        let value = null;
+        const getValue = () => value;
+        const setValue = function(toBeSet) {
+            value = toBeSet;
         }
-        str += "\n";
+        return {
+            getValue,
+            setValue
+        };
     }
-    console.log(str);
-}
-function fillBoard(symbol, r, c) {
-    if (r >= dimensions || c >= dimensions || r < 0 || c < 0) {
-        return false;
-    } else {
-        gameboard[r][c] = symbol;
-        return true;
-    }
-}
-function updateIfNeeded() {
-    if (checkRowWin() || checkColWin() || checkDiagonalWin()) {
-        gameStatus = 1;
-        return true;
-    } else if (checkDrawedGame()) {
-        gameStatus = 2;
-        return true;
-    }
-    return false;
-}
-function playRound(r, c) {
-    if (gameStatus !== 0) {
-        console.log("game is over, reset to play again!");
-    }
-    fillBoard(currPlayer.getSymbol(), r, c);
-    const flag = updateIfNeeded();
-    printBoard();
-    if (gameStatus === 1) {
-        console.log(`player ${currPlayer.getName()} wins!`);
-    } else if (gameStatus === 2) {
-        console.log("the game is drawed, its a tie!");
-    } else {
-        changeTurn();
-        printTurn();
-    }
-}
-function changeTurn() {
-    currPlayer = currPlayer === players[0] ? players[1] : players[0];
-}
-function printTurn() {
-    console.log(`it is player ${currPlayer.getName()}'s turn`);
-}
-function resetGame() {
-    gameStatus = 0;
+    // ----------state
+    const dimensions = 3;
+    const board = new Array(dimensions);
     for (let i = 0; i < dimensions; i++) {
+        board[i] = new Array();
         for (let j = 0; j < dimensions; j++) {
-            gameboard[i][j] = null;
+            board[i].push(CellFactory());
         }
     }
-    currPlayer = players[0];
-    console.log("Game Started");
-    init();
-}
-function checkRowWin() {
-    for (let i = 0; i < dimensions; i++) {
-        let rowWin = 0;
-        for (let j = 0; j < dimensions; j++) {
-            if (gameboard[i][0] !== null && gameboard[i][j] === gameboard[i][0]) rowWin++;
-            else break;
-        }
-        if (rowWin === dimensions) return true;
+    // ----------methods
+    function getDimensions() {
+        return dimensions;
     }
-    return false;
-}
-function checkColWin() {
-    for (let j = 0; j < dimensions; j++) {
-        let colWin = 0;
+    function getValue(r, c) {
+        return board[r][c].getValue();
+    }
+    function isFilled() {
         for (let i = 0; i < dimensions; i++) {
-            if (gameboard[0][j] !== null && gameboard[i][j] === gameboard[0][j]) colWin++;
-            else break;
+            for (let j = 0; j < dimensions; j++) {
+                if (board[i][j].getValue() === null) {
+                    return false;
+                }
+            }
         }
-        if (colWin === dimensions) return true;
+        return true;
     }
-    return false;
-}
-function checkDiagonalWin() {
-    return checkBDiagonalWin() || checkFDiagonalWin();
-}
-function checkBDiagonalWin() {
-    let diagonalWin = 0;
-    for (let i = 0, j = dimensions-1; i < dimensions; i++, j--) {
-        if (gameboard[0][dimensions-1] !== null && gameboard[i][j] === gameboard[0][dimensions-1]) diagonalWin++;
-        else break;
+    function isEmpty() {
+        return !isFilled();
     }
-    if (diagonalWin === dimensions) return true;
-    return false;
-}
-function checkFDiagonalWin() {
-    let diagonalWin = 0;
-    for (let i = 0, j = 0; i < dimensions; i++, j++) {
-        if (gameboard[0][0] !== null && gameboard[i][j] === gameboard[0][0]) diagonalWin++;
-        else break;
+    function fillValue(value, r, c) {
+        if (r >= dimensions || c >= dimensions || r < 0 || c < 0) return false;
+        if (board[r][c].getValue() !== null) return false; 
+        board[r][c].setValue(value);
+        return true;
     }
-    if (diagonalWin === dimensions) return true;
-    return false;
+    function printBoard() {
+        let str = "";
+        for (let i = 0; i < dimensions; i++) {
+            for (let j = 0; j < dimensions; j++) {
+                str += board[i][j].getValue();
+                str += "\t";
+            }
+            str += "\r\n";
+        }
+        console.log(str);
+    }
+    function reset() {
+        for (let i = 0; i < dimensions; i++) {
+            for (let j = 0; j < dimensions; j++) {
+                board[i][j].setValue(null);
+            }
+        }
+    }
+    return {
+        getDimensions,
+        getValue,
+        isFilled,
+        isEmpty,
+        fillValue,
+        printBoard,
+        reset
+    }
+};
+function PlayerFactory(name, symbol) {
+    const getName = () => name;
+    const changeName = function(newName) {
+        name = newName;
+    }
+    const getSymbol = () => symbol;
+    return {
+        getName,
+        changeName, 
+        getSymbol
+    };
 }
-function checkDrawedGame() {
-    // WARNING: only run this function after confirming that all win conditions have failed
-    for (let i = 0; i < dimensions; i++) {
+function GameControllerFactory_onetime() {
+    //----------state
+    let gameStatus = 0;
+    const gameboard = GameboardFactory_onetime();
+    const dimensions = gameboard.getDimensions();
+    const players = [
+        PlayerFactory("player1", " x  "),
+        PlayerFactory("player2", " o  ")
+    ];
+    let currPlayer = players[0];
+    //----------init
+    function init() {
+        console.log("----------THE GAME HAS STARTED!!----------");
+        printRound();
+    }
+    init();
+    //----------public methods
+    function playRound(r, c) {
+        if (gameStatus !== 0) {
+            console.log("game over! Reset to play again");
+            return;
+        }
+        let flag = false;
+        flag = gameboard.fillValue(currPlayer.getSymbol(), r, c);
+        if (true === flag) {
+            flag = updateGameStatus();
+        } else {
+            console.log("Invalid input, try again");
+        }
+        if (false === flag) {
+            changeTurn();
+            printRound();
+        }
+    }
+    function resetGame() {
+        console.log("resetting game...")
+        gameboard.reset();
+        gameStatus = 0;
+        currPlayer = players[0];
+        init();
+    }
+    function changeName(index, newName) {
+        if (index !== 0 && index !== 1) {
+            return false;
+        }
+        players[index].changeName(newName);
+        return true;
+    }
+    //----------return statement for GameControllerFactory_onetime()
+    return {
+        playRound,
+        resetGame,
+        changeName
+    };
+    //----------private methods
+    function changeTurn() {
+        currPlayer = currPlayer === players[0] ? players[1] : players[0];
+    }
+    function printRound() {
+        gameboard.printBoard();
+        console.log(`it is ${currPlayer.getName()}'s turn`);
+    }
+    function updateGameStatus() {
+        if (checkRowWin() === true || checkColWin() === true || checkDiagonalWin() === true) {
+            gameStatus = 1;
+            gameboard.printBoard();
+            console.log(`Player ${currPlayer.getName()} wins!`);
+            return true;
+        }
+        if (gameboard.isFilled() === true) {
+            gameStatus = 2;
+            gameboard.printBoard();
+            console.log("It's a tie!");
+            return true;
+        }
+        return false;
+    }
+    function checkRowWin() {
+        for (let i = 0; i < dimensions; i++) {
+            const compareWithMe = gameboard.getValue(i, 0);
+            if (compareWithMe === null) continue;
+            let rowWin = 0;
+            for (let j = 0; j < dimensions; j++) {
+                const val = gameboard.getValue(i, j);
+                if (val === compareWithMe) {
+                    rowWin++;
+                }
+            }
+            if (rowWin === dimensions) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function checkColWin() {
         for (let j = 0; j < dimensions; j++) {
-            if (gameboard[i][j] === null) return false;
+            const compareWithMe = gameboard.getValue(0, j);
+            if (compareWithMe === null) continue;
+            let colWin = 0;
+            for (let i = 0; i < dimensions; i++) {
+                const val = gameboard.getValue(i, j);
+                if (val === compareWithMe) {
+                    colWin++;
+                }
+            }
+            if (colWin === dimensions) {
+                return true;
+            }
         }
+        return false;
     }
-    return true;
+    function checkDiagonalWin() {
+        let flag = checkFDiagonalWin();
+        if (flag === false) {
+            flag = checkBDiagonalWin();
+        }
+        return flag;
+    }
+    function checkFDiagonalWin() {
+        let diagonalWin = 0;
+        const compareWithMe = gameboard.getValue(0, 0);
+        if (compareWithMe === null) {
+            return false;
+        }
+        for (let i = 0, j = 0; i < dimensions; i++, j++) {
+            const val = gameboard.getValue(i, j);
+            if (compareWithMe === val) {
+                diagonalWin++;
+            } else {
+                break;
+            }
+        }
+        if (diagonalWin === dimensions) return true;
+        return false;
+    }
+    function checkBDiagonalWin() {
+        let diagonalWin = 0;
+        const compareWithMe = gameboard.getValue(0, dimensions-1);
+        if (compareWithMe === null) {
+            return false;
+        }
+        for (let i = 0, j = dimensions-1; i < dimensions; i++, j--) {
+            const val = gameboard.getValue(i, j);
+            if (compareWithMe === val) {
+                diagonalWin++;
+            } else {
+                break;
+            }
+        }
+        if (diagonalWin === dimensions) return true;
+        return false;
+    }
 }
-// init
-function init() {
-    printBoard();
-    printTurn();
-}
-init();
-
+const gamecontroller = GameControllerFactory_onetime();
+// tests
 function test1() {
-    playRound(0, 0);
-    playRound(0, 1);
-    playRound(1, 1);
-    playRound(1, 2);
-    playRound(2, 2);
+    gamecontroller.playRound(0, 0);
+    gamecontroller.playRound(0, 1);
+    gamecontroller.playRound(1, 1);
+    gamecontroller.playRound(1, 2);
+    gamecontroller.playRound(2, 2);
     // result must be a win for player1
 }
 function test2() {
-    playRound(0, 1);
-    playRound(0, 0);
-    playRound(0, 2);
-    playRound(1, 1);
-    playRound(1, 0);
-    playRound(2, 2);
+    gamecontroller.playRound(0, 1);
+    gamecontroller.playRound(0, 0);
+    gamecontroller.playRound(0, 2);
+    gamecontroller.playRound(1, 1);
+    gamecontroller.playRound(1, 0);
+    gamecontroller.playRound(2, 2);
     // result must be a win for player2
 }
 function test3() {
-    playRound(1, 1);
-    playRound(1, 0);
-    playRound(2, 2);
-    playRound(0, 0);
-    playRound(2, 0);
-    playRound(2, 1);
-    playRound(1, 2);
-    playRound(0, 2);
-    playRound(0, 1);
+    gamecontroller.playRound(1, 1);
+    gamecontroller.playRound(1, 0);
+    gamecontroller.playRound(2, 2);
+    gamecontroller.playRound(0, 0);
+    gamecontroller.playRound(2, 0);
+    gamecontroller.playRound(2, 1);
+    gamecontroller.playRound(1, 2);
+    gamecontroller.playRound(0, 2);
+    gamecontroller.playRound(0, 1);
     // result must be a draw
 }
 function runtests() {
@@ -185,12 +275,12 @@ function runtests() {
     console.log("------------------test1-------------------");
     console.log("--------win for player 1 expected---------");
     test1();
-    resetGame();
+    gamecontroller.resetGame();
     console.log("------------------------------------------");
     console.log("------------------test2-------------------");
     console.log("--------win for player 2 expected---------");
     test2();
-    resetGame();
+    gamecontroller.resetGame();
     console.log("------------------------------------------");
     console.log("------------------test3-------------------");
     console.log("--------------draw expected---------------");
